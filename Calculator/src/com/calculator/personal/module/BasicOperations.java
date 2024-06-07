8b3d745575f1f2e8c2b1d6ecb764a1420f702cf5
LimbergVC <limbervillcacoraite@gmail.com>
2024-06-06 22:38:25 -0400

    add: mult

--- Calculator/src/com/calculator/personal/module/BasicOperations.java
+++ Calculator/src/com/calculator/personal/module/BasicOperations.java
@@ -4,5 +4,11 @@ public class BasicOperations {
     public double add(double firstNumber, double secondNumber) {
         return firstNumber + secondNumber;
     }
+    public double subtract(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+    public double multiply(double firstNumber, double secondNumber) {
+        return firstNumber * secondNumber;
+    }
 
 }
